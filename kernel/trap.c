8dd68c9d1aa51ccf8213101aeb492a341e1f9a43
DOGGY-SAINT <1123117528@qq.com>
2021-08-16 19:26:34 +0800

    my solution for lab 5 lazy exercise 2 lazy allocation(no test maybe)

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,22 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if(r_scause() == 13||r_scause() == 15){
+    uint64 va=r_stval();
+    printf("page trap %p\n",va);
+    va=PGROUNDDOWN(va);
+    uint64 ka=(uint64)kalloc();
+    if(ka==0)
+      p->killed=1;
+    else
+    { //ref uvmalloc
+      memset((void*)ka,0,PGSIZE);
+      if(mappages(p->pagetable, va, PGSIZE, (uint64)ka, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+      kfree((void*)ka);
+      uvmdealloc(p->pagetable, va+PGSIZE, va);
+      p->killed=1;
+    }
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
